022f87969e5947dfc31b29f0134345240f2aba58
Chanuka999 <chanukaranditha99@gmail.com>
2025-09-26 00:24:14 +0530

    refactor server.js to use dynamic PORT variable and ensure dotenv configuration is loaded

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,16 @@
 import express from "express";
 import noteRoutes from "./routes/noteRoutes.js";
 import { ConnectDb } from "./config/db.js";
+import dotenv from "dotenv";
+
 const app = express();
+dotenv.config();
+const PORT = process.env.PORT || 5001;
 
 app.use("/api/notes", noteRoutes);
 
 ConnectDb();
 
-app.listen(5001, () => {
+app.listen(PORT, () => {
   console.log("server running on PORT : 5001");
 });

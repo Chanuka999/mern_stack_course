1ea3739591ab038ba486cdc365495e8b30172d01
Chanuka999 <chanukaranditha99@gmail.com>
2025-09-26 00:24:40 +0530

    update database connection to use MONGO_URL from environment variables

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,9 +2,10 @@ import mongoose from "mongoose";
 
 export const ConnectDb = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/notedb");
+    await mongoose.connect(process.env.MONGO_URL);
     console.log("Mongodb connected successfully");
   } catch (error) {
     console.log("mongodb connected faild", error);
+    process.exit(1);
   }
 };
